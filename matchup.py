0c25c0c41f51c045d5daa7f4bf471c89686164cb
jslevinson-cloud <59921365+jslevinson-cloud@users.noreply.github.com>
2022-11-21 16:50:58 -0500

    Find word in verse

--- matchup.py
+++ matchup.py
@@ -1,7 +1,14 @@
-import sys
+import sys, csv
+
 word = 1
 while word != "":
     word = input("Enter Word\n")
-    if word == "": sys.exit()
+    if word == "":
+        sys.exit()
     print(word)
-
+    with open("kjv.csv", "r") as file:
+        kjv = csv.reader(file)
+        for lines in kjv:
+            if len(lines) > 2:
+                if lines[5].find(word) != -1:
+                    print(lines[5])
