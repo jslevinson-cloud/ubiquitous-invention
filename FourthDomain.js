c1796492d09c688eec58a8288fbb125ca2475c60
jslevinson-cloud <59921365+jslevinson-cloud@users.noreply.github.com>
2022-11-30 10:44:36 -0500

    Solve CORS problems in accessing local file

--- FourthDomain.js
+++ FourthDomain.js
@@ -1,8 +1,32 @@
 function lookupWord() {
-    alert('here')
     var text = document.getElementById('TEXTBOX_ID').value;
-    alert(text)
-    kjv = "kjv.csv"
-    var data = $.csv.toObjects(kjv)
-    alert(data)
+    kjv = "http:localhost:8000/kjv.csv"
+    readTextFile(kjv)
+    /*
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+        kjv.src = event.target.result;
+        var data = $.csv.toObjects(kjv.src)
+        alert(data.length)
+        alert(data[0])
+    
+    });
+    */
+}
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
