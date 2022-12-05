91578d3b30622b65dca6b626e7c519274f2e4186
jslevinson-cloud <59921365+jslevinson-cloud@users.noreply.github.com>
2022-12-05 14:25:22 -0500

    use textarea

--- FourthDomain.js
+++ FourthDomain.js
@@ -1,5 +1,6 @@
+var lookup;
 function lookupWord() {
-    var text = document.getElementById('TEXTBOX_ID').value;
+    lookup = document.getElementById('TEXTBOX_ID').value;
     kjv = "http:localhost:8000/kjv.csv"
     readTextFile(kjv)
     /*
@@ -17,25 +18,59 @@ function readTextFile(file)
 {
      var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
+    console.log("lookup1="+lookup);
     rawFile.onreadystatechange = function ()
     {
+        console.log("lookup2 = "+lookup);
         if(rawFile.readyState === 4)
         {
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                console.log(allText);
+                //console.log(allText);
+                datasplit = allText.split("\n");
+                for (p in datasplit) {
+                    //reference = datasplit[p].split(",")[0];
+                    text = datasplit[p].split(",")[5];
+                    //console.log("reference="+reference)
+                    //console.log(String(text));
+                    if (String(text).search(lookup) != -1) {
+                        var newText = document.createTextNode(text+'\n');
+                        var textarea = document.getElementById('outputkjv');
+                        textarea.appendChild(newText);
+                        console.log("text="+text);
+                    }
+                    /*
+                    if (Number.isInteger(Number(reference))&&Number(reference)!=0) {
+                        var data = $.csv.toObjects(datasplit[p]);
+                        console.log(data);  
+                    }
+                    */
+                    
+                }
+                //console.log(Object.getOwnPropertyNames(datasplit));
                 //alert(allText);
-                var data = $.csv.toObjects(allText);
+                //var data = $.csv.toObjects(allText);
                 var textarea = document.getElementById("outputkjv")
                 //alert(data[0][0]);
                //console.log(data);
+               //console.log(typeof datasplit);
+               //console.log(Object.getOwnPropertyNames(datasplit));
+               /*
                 for (i=0; i < data.length; i++) {
-                    textarea.value +=  data[i];
-                    if (data[i].length >= 2) {
-                        textarea.value += data[i][5];
+                    //console.log(data[i].toObject());
+                    for (j=0; j < data[i].length; j++) {
+                        for (p in data[i][j]) {
+                            console.log(p);
+                        }
                     }
+                    //textarea.value +=  data[i];
+                    //if (data[i].length >= 2) {
+                        //textarea.value += data[i][5];
+                        //console.log("%o", data[i]);
+                    //}
                 }
+                */
   
             }
         }
