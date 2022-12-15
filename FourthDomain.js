a2df440fd18f45f51a369b7cf260749024570531
jslevinson-cloud <59921365+jslevinson-cloud@users.noreply.github.com>
2022-12-15 15:42:35 -0500

    working but slow

--- FourthDomain.js
+++ FourthDomain.js
@@ -1,40 +1,55 @@
-var lookup;
- 
 function lookupWord() {
-    lookup = document.getElementById('TEXTBOX_ID').value;
-    if (lookup == "") {
+    var lookup = document.getElementById("TEXTBOX_ID").value;
+    var textarea;
+    var thesaurus;
+    var kjv;
+    var thesaurus_text;
+    var allText;
+
+    if (lookup === "") {
         return;
     }
-    textarea = document.getElementById('outputkjv');
+    textarea = document.getElementById("outputkjv");
     //textarea.value = "";
-    thesaurus = "http:localhost:8000/Thesaurus/WordnetThesaurus.csv"
+    thesaurus = "http:localhost:8000/Thesaurus/WordnetThesaurus.csv";
     kjv = "http:localhost:8000/kjv.csv";
-    thesaurus_text = readThesaurus(thesaurus)
-    allText = readTextFile(kjv);
-    processTextFile(textarea, allText, thesaurus_text);
+    thesaurus_text = readFile(thesaurus);
+    allText = readFile(kjv);
+    processTextFile(lookup, textarea, allText, thesaurus_text);
  }
 
-function processTextFile(textarea, allText, thesaurus)
+function processTextFile(lookup, textarea, allText, thesaurus)
 {
-    thesaurus += lookup +'\n';
     var thesaurus_split = thesaurus.split("\n");
-    for (prop in thesaurus_split) {
-        thesaurus_line = thesaurus_split[prop];
-        if (typeof(thesaurus_line) != 'undefined' && thesaurus_line.toUpperCase().search(lookup.toUpperCase()) != -1) {
-            console.log(thesaurus_line);
+    var prop;
+    var p;
+    var thesaurus_line;
+
+    thesaurus_split[thesaurus_split.length] = lookup;
+    datasplit = allText.split("\n");
+    seen = {};
+    for (i = 0; i < thesaurus_split.length; i++) {
+        thesaurus_line = thesaurus_split[i];
+        if ((typeof(thesaurus_line) !== "undefined") && thesaurus_line.toUpperCase().search(lookup.toUpperCase()) !== -1) {
+            //console.log(thesaurus_line);
             words = thesaurus_line.split(",");
-            for (word in words) {
+            for (j = 0; j < words.length; j++) {
+                word = words[j];
+                if (seen[word] != 1) {
                 //console.log("word="+words[word]);
-                datasplit = allText.split("\n");
-                for (p in datasplit) {
-                    //reference = datasplit[p].split(",")[0];
-                    text = datasplit[p].split(",")[5];
-                    if (typeof(text) != 'undefined' && String(text.toUpperCase()).search(words[word].toUpperCase()) != -1) {
-                        var newText = document.createTextNode(text+'\n');
-                        //textarea.appendChild(newText);
-                        console.log("text="+text);
+                //console.log("j="+j+" word="+word);
+                    for (k = 0; k < datasplit.length; k++) {
+                        //reference = datasplit[p].split(",")[0];
+                        text = datasplit[k].split(",")[5];
+                        if ((typeof(text) !== "undefined" && text.length > 0 && word.length > 0) 
+                            && text.toUpperCase().search(" " + word.toUpperCase()+" ") !== -1) {
+                            var newText = document.createTextNode(text+" word="+word+"\n");
+                            textarea.appendChild(newText);
+                            //console.log("text="+text+" word="+word);
+                        }
                     }
                 }
+                seen[word] = 1;
             }
         }
 
@@ -42,16 +57,17 @@ function processTextFile(textarea, allText, thesaurus)
 
 }
 
-function readTextFile(file)
+
+function readFile(url)
 {
     var rawFile = new XMLHttpRequest();
     var allText;
-    rawFile.open("GET", file, false);
+    rawFile.open("GET", url, false);
     rawFile.onreadystatechange = function ()
     {
         if(rawFile.readyState === 4)
         {
-            if(rawFile.status === 200 || rawFile.status == 0)
+            if(rawFile.status === 200 || rawFile.status === 0)
             {
                     allText = rawFile.responseText;
             }
@@ -61,22 +77,4 @@ function readTextFile(file)
     return allText;
 }
 
-function readThesaurus(thesaurus)
-{
-    var rawFile = new XMLHttpRequest();
-    var allText;
-    rawFile.open("GET", thesaurus, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                allText = rawFile.responseText;
-            }
-        }
-    }
-    rawFile.send(null);
-    return allText;
-}
 
